c4e4e8a22e8f75991beaa8cb42278443635c31c7
ontofractal <v@ontofractal.com>
2020-06-30 20:49:48 +0300

    package config updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2020": true
-    },
-    "extends": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-      "prefer-arrow-callback": "warn"
-    }
-};
+  "env": {
+    "browser": true,
+    "es2020": true
+  },
+  "extends": [],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": 11,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "prefer-arrow-callback": "warn",
+    "semi": ["warn", "never"]
+  }
+}
