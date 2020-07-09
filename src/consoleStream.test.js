7d81c14271e96f24a362d28ce20b5774ffa57406
Jack Williams <jpwilliamsphotography@gmail.com>
2020-07-09 17:03:40 +0100

    test(consoleStream): rewrite to better test process.stdout

--- src/consoleStream.test.js
+++ src/consoleStream.test.js
@@ -1,29 +1,23 @@
-import consoleStream from './consoleStream'
+import { mockProcessStdout } from 'jest-mock-process'
 
-describe("Console stream", () => {
-  let consoleLogData = ""
-  const storeLog = (inputs) => (consoleLogData += inputs)
-  it("streams correctly", async (done) => {
-    console["log"] = jest.fn(storeLog)
+describe('Console stream', () => {
+  let consoleStream
+  let mockStdout
 
-    const stream = consoleStream({})
-    stream.write(JSON.stringify({id: 1, name: `item first`}))
+  beforeAll(() => {
+    mockStdout = mockProcessStdout()
+    consoleStream = require('./consoleStream').default
+  })
 
+  it('streams correctly', () => {
+    const stream = consoleStream()
+    stream.write(
+      JSON.stringify({ id: 1, name: `item first`, time: 1594310416073 })
+    )
     stream.end()
 
-    const parsedLogData = JSON.parse(consoleLogData)
-
-    expect(parsedLogData).toMatchObject({
-        "metadata": {
-          "context": {},
-          "id": 1,
-          "name": "item first",
-          "level": "info"
-        },
-        "message": "info"
-      }
+    expect(mockStdout).toHaveBeenCalledWith(
+      '{"metadata":{"id":1,"name":"item first","context":{},"level":"info"},"message":"info","timestamp":1594310416073}\n'
     )
-    done()
   })
 })
-
