74aebe26b3b308a60d0c57d2c32a759ee0f9f9a3
chasers <chase@logflare.app>
2021-07-14 09:01:16 -0700

    Try Level or number

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ const isNode =
 const createPinoBrowserSend = (options: LogflareUserOptionsI) => {
   const client = new LogflareHttpClient({ ...options, fromBrowser: true })
 
-  return (level: Level, logEvent: LogEvent) => {
+  return (level: Level | number, logEvent: LogEvent) => {
     const logflareLogEvent = formatPinoBrowserLogEvent(logEvent)
     const maybeWithTransforms = addLogflareTransformDirectives(
       logflareLogEvent,
